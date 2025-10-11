772c772a73e664428b4e84ee35e4cbe52f11e0a0
FrancyellyMonteiro <souza.francyellymonteiro@gmail.com>
2025-10-11 11:40:20 -0300

    Add files via upload

---
+++ IPE_1011/roteiro09/AgendaSimples.py
@@ -0,0 +1,15 @@
+Dados = {}
+
+Endereco = input("Informe o seu endereço: ")
+Telefone = input("Informe seu telefone: ")
+
+Dados ["Endereço"] = Endereco 
+Dados ["Telefone"] = Telefone
+
+
+def ledados():
+    print(Dados)
+ledados()
+
+Agenda = {'Paulo': 123}
+print(Agenda)
