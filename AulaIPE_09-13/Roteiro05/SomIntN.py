245c7440bfccf53eb777cc4fe29e7aa9bf80d318
FrancyellyMonteiro <souza.francyellymonteiro@gmail.com>
2025-10-30 02:04:40 -0300

    Delete AulaIPE_09-13 directory

--- AulaIPE_09-13/Roteiro05/SomIntN.py
+++
@@ -1,9 +0,0 @@
-limite = int(input("Digite um numero inteiro positivo: "))
-resposta = 0
-
-if limite > 0:
-    for n in range(limite+1):
-        resposta = resposta+n
-        print(resposta)
-else:
-    print("O numero que vc digitou não é um numero inteiro positivo")
